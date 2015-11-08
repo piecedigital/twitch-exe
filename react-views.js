9607f77e80e9c9d3b6d7e27440cc2695513efcdc
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-11-08 03:00:06 -0500

    working out component pieces

--- react-views.js
+++ react-views.js
@@ -1,74 +1,71 @@
 //////////////////////////////
 // app ///////////////////////
 //////////////////////////////
-function topGames() {
-  var TopGames = React.createClass({
-    displayName: "TopGames",
+var TopGames = React.createClass({
+  displayName: "TopGames",
 
-    getGames: function() {
-      var classInstance = this;
-      ajax({
-        url: "https://api.twitch.tv/kraken/games/top",
-        success: function(data) {
-          classInstance.setState({games: JSON.parse(data.target.response)});
-          console.log(JSON.parse(data.target.response));
-          console.log(classInstance)
-        },
-        error: function(data) {
-          console.log(data)
-        }
-      });
-    },
-    getInitialState: function() {
-      return { games : {} };
-    },
-    componentDidMount: function() {
-      this.getGames();
-    },
-    componentWillUpdate: function(nextProps, nextState) {
-      console.log(nextState)
-    },
-    render: function render() {
-      if(!this.state.games.top) {
-        return false;
+  getGames: function() {
+    var classInstance = this;
+    ajax({
+      url: "https://api.twitch.tv/kraken/games/top",
+      success: function(data) {
+        classInstance.setState({games: JSON.parse(data.target.response)});
+        //console.log(JSON.parse(data.target.response));
+        //console.log(classInstance)
+      },
+      error: function(data) {
+        console.log(data)
       }
-      return React.createElement(
-        "ul",
-        { "className" : "games-list" },
-        this.state.games.top.map(function(item, ind) {
-          return React.createElement(
-            "li",
-            { "key" : item + ind, "className" : "game-item" },
-            React.createElement(
-              "h1",
-              null,
-              item.game.title
-            ),
-            React.createElement(
-              "img",
-              { "src" : item.game.box.small }
-            ),
+    });
+  },
+  getInitialState: function() {
+    return { games : {} };
+  },
+  componentDidMount: function() {
+    this.getGames();
+  },
+  componentWillUpdate: function(nextProps, nextState) {
+    //console.log(nextState)
+  },
+  render: function render() {
+    if(!this.state.games.top) {
+      return false;
+    }
+    return React.createElement(
+      "ul",
+      { "className" : "games-list" },
+      this.state.games.top.map(function(item, ind) {
+        return React.createElement(
+          "li",
+          { "key" : item + ind, "className" : "game-item" },
+          React.createElement(
+            "h1",
+            null,
+            item.game.title
+          ),
+          React.createElement(
+            "img",
+            { "src" : item.game.box.small }
+          ),
+          React.createElement(
+            "span",
+            { "className" : "stats" },
             React.createElement(
               "span",
-              { "className" : "stats" },
-              React.createElement(
-                "span",
-                null,
-                item.viewers
-              ),
-              "/",
-              React.createElement(
-                "span",
-                null,
-                item.channels
-              )
+              null,
+              item.viewers
+            ),
+            "/",
+            React.createElement(
+              "span",
+              null,
+              item.channels
             )
           )
-        })
-      );
-    }
-  });
+        )
+      })
+    );
+  }
+});
 
-  ReactDOM.render(React.createElement(TopGames, null), document.getElementById("online-streams"));
-}
-topGames();
+ReactDOM.render(React.createElement(TopGames, null), document.getElementById("online-streams"));
