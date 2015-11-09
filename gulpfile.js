89f8a743b12bde8cf56e9d05a2e93c6edb380fd1
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-11-09 04:26:06 -0500

    creating react views

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require("gulp");
+//var browserSync = require("browser-sync").create();
+var run = require("gulp-run");
+//var reload = browserSync.reload;
+var sass = require('node-sass');
+var fs = require("fs");
+
+function renderSass() {
+	sass.render({
+	  file: "./main.scss",
+	  outputStyle: "nested",
+	  outFile: "./main.css"
+	}, function(err, result) {
+		if(err) throw err;
+
+		//console.log(result.css.toString());
+		fs.writeFile('./main.css', result.css.toString(), function (err) {
+		  if (err) throw err;
+
+		  console.log('CSS rendered and saved');
+		});
+	});
+}
+
+gulp.task("start", function() {
+	run("electron .").exec();
+});
+
+gulp.task("serve", function() {
+	gulp.watch(["*.scss"]).on("change", renderSass);
+	//gulp.watch(["*.html", "*.css"]).on("change", reload);
+});
+
+gulp.task("default", ["start", "serve"]);
--- react-views.js
+++ react-views.js
@@ -1,17 +1,56 @@
 //////////////////////////////
 // app ///////////////////////
 //////////////////////////////
+var separator = React.createElement(
+  "span",
+  { className : "separator" },
+  React.createElement(
+    "span",
+    null
+  )
+)
+var smallSeparator = React.createElement(
+  "span",
+  { className : "small-separator" },
+  React.createElement(
+    "span",
+    null
+  )
+)
+var HomePage = React.createClass({
+  displayName: "HomePage",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { "id" : "front-page" },
+      React.createElement(
+        "h1",
+        { className : "unset section-title x-pad-1_0" },
+        "Top Games"
+      ),
+      React.createElement(TopGames),
+      separator,
+      React.createElement(
+        "h1",
+        { className : "unset section-title x-pad-1_0" },
+        "Top Streams"
+      ),
+      React.createElement(TopStreams)
+    );
+  }
+});
+
 var TopGames = React.createClass({
   displayName: "TopGames",
 
   getGames: function() {
     var classInstance = this;
     ajax({
-      url: "https://api.twitch.tv/kraken/games/top",
+      url: "https://api.twitch.tv/kraken/games/top?limit=12",
       success: function(data) {
         classInstance.setState({games: JSON.parse(data.target.response)});
-        //console.log(JSON.parse(data.target.response));
-        //console.log(classInstance)
+        console.log("Top Games", JSON.parse(data.target.response));
       },
       error: function(data) {
         console.log(data)
@@ -33,19 +72,19 @@ var TopGames = React.createClass({
     }
     return React.createElement(
       "ul",
-      { "className" : "games-list" },
+      { "id" : "games-list", "className" : "unset x-pad-1_0 center-justify" },
       this.state.games.top.map(function(item, ind) {
         return React.createElement(
           "li",
-          { "key" : item + ind, "className" : "game-item" },
-          React.createElement(
-            "h1",
-            null,
-            item.game.title
-          ),
+          { "key" : "game-item" + ind, "className" : "game-item col-6-5-4-3-2-1" },
           React.createElement(
             "img",
-            { "src" : item.game.box.small }
+            { "src" : item.game.box.large }
+          ),
+          React.createElement(
+            "h1",
+            { "className" : "title" },
+            `${item.game.name}`
           ),
           React.createElement(
             "span",
@@ -53,13 +92,72 @@ var TopGames = React.createClass({
             React.createElement(
               "span",
               null,
-              item.viewers
+              `Viewers: ${item.viewers}`
             ),
-            "/",
+            smallSeparator,
             React.createElement(
               "span",
               null,
-              item.channels
+              `Channels: ${item.channels}`
+            )
+          )
+        )
+      })
+    );
+  }
+});
+var TopStreams = React.createClass({
+  displayName: "TopStreams",
+
+  getStreams: function() {
+    var classInstance = this;
+    ajax({
+      url: "https://api.twitch.tv/kraken/streams/featured?limit=6",
+      success: function(data) {
+        classInstance.setState({streams: JSON.parse(data.target.response)});
+        console.log("Top Streams", JSON.parse(data.target.response));
+      },
+      error: function(data) {
+        console.log(data)
+      }
+    });
+  },
+  getInitialState: function() {
+    return { streams : {} };
+  },
+  componentDidMount: function() {
+    this.getStreams();
+  },
+  componentWillUpdate: function(nextProps, nextState) {
+    //console.log(nextState)
+  },
+  render: function render() {
+    if(!this.state.streams.featured) {
+      return false;
+    }
+    return React.createElement(
+      "ul",
+      { "id" : "streams-list", "className" : "unset x-pad-1_0" },
+      this.state.streams.featured.map(function(item, ind) {
+        return React.createElement(
+          "li",
+          { "key" : "stream-item" + ind, "className" : "stream-item inline-block col-3-2-1" },
+          React.createElement(
+            "img",
+            { "src" : item.stream.preview.large }
+          ),
+          React.createElement(
+            "h1",
+            { "className" : "title"},
+            `${item.title}`
+          ),
+          React.createElement(
+            "span",
+            { "className" : "stats" },
+            React.createElement(
+              "span",
+              null,
+              `${item.stream.viewers} viewers on ${item.stream.channel.display_name}`
             )
           )
         )
@@ -68,4 +166,4 @@ var TopGames = React.createClass({
   }
 });
 
-ReactDOM.render(React.createElement(TopGames, null), document.getElementById("online-streams"));
+ReactDOM.render(React.createElement(HomePage, null), document.getElementById("main-content"));
